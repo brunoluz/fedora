8cba5ab2c2471d63e9c3c3b63874e71930fe3cc7
Bruno Luz <bruno@localhost.localdomain>
2020-09-09 13:58:38 -0300

    more settings

--- install-and-configure-environment.sh
+++ install-and-configure-environment.sh
@@ -1,1 +1,11 @@
 sudo dnf install xclip -y
+sudo dnf install gnome-tweak-tool -y
+
+# nautilus - add "New Document" or right click
+touch ~/Templates/Empty\ Document
+
+# install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm && sudo dnf localinstall google-chrome-stable_current_x86_64.rpm -y && rm -f google-chrome-stable_current_x86_64.rpm*
+
+# tap to click
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
