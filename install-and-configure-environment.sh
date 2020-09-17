82bbd444411700eefb39730eaf2d184db2b4fd82
Bruno Luz <bruno.al.luz@gmail.com>
2020-09-16 22:12:03 -0300

    install cascadiacode font

--- install-and-configure-environment.sh
+++ install-and-configure-environment.sh
@@ -8,7 +8,15 @@ sudo dnf install dnfdragora -y
 touch ~/Templates/Empty\ Document
 
 # install chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm && sudo dnf localinstall google-chrome-stable_current_x86_64.rpm -y && rm -f google-chrome-stable_current_x86_64.rpm*
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm && \
+sudo dnf localinstall google-chrome-stable_current_x86_64.rpm -y && \
+rm -f google-chrome-stable_current_x86_64.rpm*
 
 # tap to click
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+
+# install cascadia code font
+wget https://github.com/microsoft/cascadia-code/releases/download/v2009.14/CascadiaCode-2009.14.zip && \
+sudo mkdir -p /usr/share/fonts/truetype && \
+sudo unzip CascadiaCode-2009.14.zip -d /usr/share/fonts/truetype && \
+rm -f CascadiaCode-2009.14.zip*
