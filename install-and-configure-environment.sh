80473cb82b8fafb12d32f72a21149dd7c5be81ed
Bruno Luz <bruno.al.luz@gmail.com>
2020-09-24 23:12:52 -0300

    nautilus backspace

--- install-and-configure-environment.sh
+++ install-and-configure-environment.sh
@@ -69,3 +69,6 @@ echo "xbindkeys" >> ~/.profile
 
 # open with code (https://github.com/harry-cpp/code-nautilus)
 wget -qO- https://raw.githubusercontent.com/cra0zy/code-nautilus/master/install.sh | bash
+
+# backspace shortcut for nautilus (https://github.com/7aman/backspace-up)
+wget -qO- https://raw.githubusercontent.com/7aman/backspace-up/master/install.sh | bash
