be79ee16320f4cfa5cbd3b99b2d091622751be51
Bruno Luz <bruno.al.luz@gmail.com>
2020-09-26 17:06:25 -0300

    imwheel

--- install-and-configure-environment.sh
+++ install-and-configure-environment.sh
@@ -69,3 +69,14 @@ echo "xbindkeys" >> ~/.profile
 
 # open with code (https://github.com/harry-cpp/code-nautilus)
 wget -qO- https://raw.githubusercontent.com/cra0zy/code-nautilus/master/install.sh | bash
+
+# imwhell 
+sudo dnf install imwheel -y
+echo "\".*\"
+None,      Up,   Button4, 2
+None,      Down, Button5, 2
+Control_L, Up,   Control_L|Button4
+Control_L, Down, Control_L|Button5
+Shift_L,   Up,   Shift_L|Button4
+Shift_L,   Down, Shift_L|Button5" >> ~/.imwheelrc
+echo "imwheel" >> ~/.profile
